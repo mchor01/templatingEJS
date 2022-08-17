1a036594a41e0e70386b87abc45041af85310e62
mchor01 <81789617+mchor01@users.noreply.github.com>
2022-08-17 05:31:57 +0000

    final

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  var title = "My Home Page";
+  var heading = "My Website";
+  res.render('pages/index',{ 
+    'title':title,
+    'heading':heading
+  })
+
+})
+app.get('/about', (req, res) => {
+  var title = "My About Page";
+  var heading = "My Website";
+
+  res.render('pages/about',{ 
+    'title':title,
+    'heading':heading
+  })
+})
+  app.get('/contact', (req, res) => {
+  var title = "My Contact Page";
+  var heading = "My Website";
+  res.render('pages/contact',{ 
+    'title':title,
+    'heading':heading
+  })
+  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
